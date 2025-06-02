7e344f4e4836308416bb298944531148b4747c9b
Antony-hr <82633900+Antony-hr@users.noreply.github.com>
2025-06-01 21:23:14 -0600

    fix: specify port for Vite preview command

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/react-image-gallery/",
 	resolve: {
 		alias: [
 			{ find: "@", replacement: resolve(__dirname, "./src") },
